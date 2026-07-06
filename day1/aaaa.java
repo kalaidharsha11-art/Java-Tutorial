f3a56693dcc2da6714f0bbb14ea33e2eb6a270ab
kalaidharsha11-art <kalaidharsha11@gmail.com>
2026-07-06 12:02:35 +0530

    Add files via upload

---
+++ day1/aaaa.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class aaaa {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Positive Number");
+        } else {
+            System.out.println("Negative or Zero");
+        }
+    }
+}
---
+++ day1/ddd.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class ddd {
+    public static void main(String[] args) {
+        int a = 10, b = 25, c = 15;
+
+        if (a > b && a > c) {
+            System.out.println("A is the largest");
+        } else if (b > a && b > c) {
+            System.out.println("B is the largest");
+        } else {
+            System.out.println("C is the largest");
+        }
+    }
+}
